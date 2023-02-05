300cb92f8e9bc1149a2d69ce2887c733ad60a589
Adam Bergstrom <abx1138@gmail.com>
2023-02-05 17:55:09 -0600

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import './App.css';
 import Canvas from './Components/Canvas';
 
+import React from "react";
+import ReactCanvasPaint from 'react-canvas-paint'
+import 'react-canvas-paint/dist/index.css'
+
+// const { createCanvas } = require('canvas');
+
+// const width = 1200;
+// const height = 600;
+
+// const canvas = createCanvas(width, height);
+// const context = canvas.getContext('2d');
+
+// context.fillStyle = '#fff';
+// context.fillReact(0, 0, width, height)
+
+
+// import React from "react";
+// import ReactCanvasPaint from 'react-canvas-paint'
+// import 'react-canvas-paint/dist/index.css'
+
+// export default function DrawingCanvasGfg(){
+// return (
+// 	<div>
+// 	<h1>ReactJs Drawing Canvas - GeeksforGeeks</h1>
+// 	<ReactCanvasPaint />
+// 	</div>
+// );
+// };
+
+
 function App() {
-  return (
+  return (<>
+    <h1>Drawing App</h1>
     <div className="App">
+
       <Canvas
         width={700}
         height={500}
       />
+
     </div>
+    </>
   );
+
 }
 
 export default App;
